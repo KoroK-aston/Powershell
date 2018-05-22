7ad934945c55e4e83ee4de83ea3637ee2e860118
KoroK-aston <korok8192.314@protonmail.com>
2018-05-22 14:43:59 +0200

    New

---
+++ Calculatrice.ps1
@@ -0,0 +1,16 @@
+﻿[int]$Value1 = read-host "Enter a value"
+$operator = Read-Host "Choose an operator to apply"
+[int]$value2 = Read-Host "Enter a second value"
+
+
+
+#Fonction calcul 
+    switch ($operator) {
+   "+" {$result = $Value1 + $value2}
+   "-" {$result = $Value1 - $value2}
+   "*" {$result = $Value1 * $value2}
+   "/" {$result = $Value1 / $value2}
+   }
+
+
+Write-Output $result
---
+++ Shifumi.ps1
@@ -0,0 +1,32 @@
+﻿$Choix = @("Pierre", "Feuille", "Ciseaux")
+
+
+Write-output "Choix possible"
+echo @("1 : Pierre", "2 : Feuille" , "3 : Ciseaux") `n
+
+$ChoixUser = Read-Host "Faites un choix"
+Switch ($ChoixUser) {
+    "1" {$ChoixUser = $Choix[0]} 
+    "2" {$ChoixUser = $Choix[1]}
+    "3" {$ChoixUser = $Choix[2]}
+    }
+Write-Output "Vous jouez :" $ChoixUser `n
+
+Sleep 2
+
+$ChoixCPU = Get-Random $Choix
+Write-Output "Machine joue :" $ChoixCPU `n
+
+Sleep 2
+
+#Compteur
+    If ($ChoixUser -like $Choix[0] -and $ChoixCPU -like $Choix[1]) {
+
+    Write-Output "Machine gagne"
+    
+    }
+        Else {
+
+        Write-Output "Joueur gagne"
+    
+    }
